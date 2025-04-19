7d68af69b2f870d3655c5b452f876bb19de497e8
Yanice910 <leeyx-pm23@student.tarc.edu.my>
2025-04-19 16:34:04 +0800

    1

--- GUI assignment/src/java/registerServlet.java
+++ GUI assignment/src/java/registerServlet.java
@@ -2,7 +2,9 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet(urlPatterns= {"/registerServlet"})
 public class registerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
         String username = request.getParameter("username");
